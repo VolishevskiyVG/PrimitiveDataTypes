9d73b89dc05a8e836cf022077da4367d4e08a70e
Vasiliy Volishevskiy <Volishevski.V@gmail.com>
2022-11-17 15:48:36 +0300

    различные операции

---
+++ src/test/java/PrimitiveDataTypes.java
@@ -0,0 +1,33 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte varByte = 127;
+        short varShort = 32767;
+        int varInt = 2147483647;
+        long varLong = 9223372036854775807L;
+        float varFloat = 127.2f;
+        double varDouble = 13.5d;
+        char varChar = 'd';
+        boolean varBoolean = false;
+
+
+        System.out.println(varByte + 6);
+        System.out.println(varInt + 2);
+        System.out.println(varLong + 7);
+        System.out.println();
+        System.out.println();
+
+
+        int a = 14;
+        int b = 5;
+        double d = 6.4;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(1.0*a/b);
+        System.out.println(a%b);
+        System.out.println(a+d);
+    }
+
+}
